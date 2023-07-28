58d2a80268178f62f35b49477ae0b646f6e4fd55
Ye0ngJae <71473974+Ye0ngjae@users.noreply.github.com>
2023-07-29 01:47:16 +0900

    Update Sensor_node.ino

--- Sensor_node.ino
+++ Sensor_node.ino
@@ -6,6 +6,8 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <ArduinoJson.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
 
 #define rxPin 3
 #define txPin 2
@@ -15,37 +17,51 @@
 
 MPU9250 mpu;
 SoftwareSerial esp01(txPin, rxPin);
-const char* ssid = "SSID";
+const char* ssid = "WiFi_SSID";
 const char* password = "Password";
-const char* serverUrl = "http://api.ye0ngjae.com"; 
+const char* serverUrl = "http://api.ye0ngjae.com/data"; // server URL
+
+WiFiUDP udp;
+NTPClient ntpClient(udp, "pool.ntp.org");
 
 void setup() {
   Wire.begin();
   Serial.begin(9600);
   esp01.begin(9600);
-
+  
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to WiFi...");
   }
   Serial.println("Connected to WiFi");
+
+  // NTP 시간 동기화
+  ntpClient.begin();
+  ntpClient.update();
 }
 
 void loop() {
+  // 현재 시간 정보 업데이트
+  ntpClient.update();
+  String currentTimestamp = ntpClient.getFormattedTime();
+
   int SoundValue = analogRead(sound);
   int WaterValue = analogRead(water);
   int VibrationValue = analogRead(vibration);
+
+  // 사운드 센서 값 (Hz 단위)
+  float soundFrequency = SoundValue * 0.1; // 예시: 10 Hz 단위로 변환
 
   // MPU-9250 Data
   mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
 
   // JSON Data 생성
   StaticJsonDocument<200> jsonData;
-  jsonData["site"] = "Your_Site_Name"; // Replace with your site name
-  jsonData["timestamp"] = "2023-07-28T12:34:56"; // Replace with current timestamp in ISO 8601 format
+  jsonData["site"] = "Site_Name"; // site name
+  jsonData["timestamp"] = currentTimestamp; // 현재 시간 정보
   jsonData["vibration"] = VibrationValue;
-  jsonData["sound"] = SoundValue;
+  jsonData["sound"] = soundFrequency; // 사운드 센서 값 (Hz 단위)
   jsonData["WaterPressure"] = WaterValue;
 
   // JSON 데이터를 문자열로 변환
@@ -72,5 +88,5 @@ void loop() {
 
   http.end();
 
-  delay(1000); // 1초마다 데이터 전송
+  delay(500); // 0.5초마다 데이터 전송
 }
