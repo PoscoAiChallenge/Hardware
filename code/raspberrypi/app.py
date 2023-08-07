bc915431ba122a82975b34c0777fc22b0ab6142c
ye0ngjae <op@ye0ngjae.com>
2023-08-07 18:37:18 +0900

    update

--- code/raspberrypi/app.py
+++ code/raspberrypi/app.py
@@ -1,15 +1,16 @@
 import flask
 import requests
+import json
+import threading
 import site1
 import site2
-import json
 from datetime import datetime
 
 app = flask.Flask(__name__)
 
 LOG_API_URL = 'https://api.ye0ngjae.com/log/'
 
-status = True
+status = False
 
 def send_log_to_server(hardware_tag, log):
     Log = {
@@ -20,39 +21,45 @@ def send_log_to_server(hardware_tag, log):
     Log = json.dumps(Log)
     requests.post(LOG_API_URL, json=Log)
 
-send_log_to_server("status","Hardware System started")
+send_log_to_server("status", "Hardware System started")
 
 @app.route('/')
 def index():
-    #return index.html
+    # return index.html
     return flask.render_template('index.html')
 
-@app.route('/on', methods=['POST'])
-def on():
-    global status
-    send_log_to_server("status","Hardware System turned on")
-    
-
-    while(True):
-        if status == False:
+# 백그라운드에서 실행할 함수
+def serial_read_task():
+    while True:
+        if not status:
             break
         try:
             site1.serial_read()
             site2.serial_read()
         except:
-            send_log_to_server("error","Failed to read serial")
+            send_log_to_server("error", "Failed to read serial")
             continue
-    
+
+@app.route('/on', methods=['POST'])
+def turn_on():
+    global status
+
+    if not status:
+        status = True
+        send_log_to_server("status", "Hardware System turned on")
+
+        # 백그라운드 스레드 시작
+        serial_thread = threading.Thread(target=serial_read_task)
+        serial_thread.start()
+
     return "Hardware System turned on"
 
-
 @app.route('/off', methods=['POST'])
-def off():
+def turn_off():
     global status
     status = False
-    send_log_to_server("status","Hardware System turned off")
+    send_log_to_server("status", "Hardware System turned off")
     return "Hardware System turned off"
-    
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
